fc1ab8d7fc4390c54fa9c03d4b834cb6da56b44d
Jmeas <jellyes2@gmail.com>
2015-01-18 22:58:28 -0500

    No longer modify original event.
    Resolves #28

--- src/time-segments.js
+++ src/time-segments.js
@@ -11,7 +11,8 @@ var TimeSegments = {
       startAttribute: 'start',
       endAttribute: 'end'
     });
-    var start, end, startIsMoment, endIsMoment;
+
+    var segments = {}, start, end, startIsMoment, endIsMoment;
     events = _.chain(events)
       .clone()
       .map(e => {
@@ -22,33 +23,28 @@ var TimeSegments = {
         if ((!startIsMoment || !endIsMoment) && !options.timeFormat) {
           throw new Error('Parsing strings into dates requires the `timeFormat` option.');
         }
-        start = moment.isMoment(start) ? start : moment(start, options.timeFormat);
-        end = moment.isMoment(end) ? end : moment(end, options.timeFormat);
-        return _.extend(e, {start, end});
+        start = moment.isMoment(start) ? start : moment.utc(start, options.timeFormat);
+        end = moment.isMoment(end) ? end : moment.utc(end, options.timeFormat);
+        return {
+          start, end,
+          original: _.clone(e)
+        };
       })
-      .sortBy(e => { return e.start.unix(); })
-      .value();
+      .sortBy(e => e.start.unix())
+      .each(e => {
+        // Calculate the duration of the event; this determines
+        // how many segments it will be in
+        var startMoment = moment.utc(e.start).startOf(scale);
+        var endMoment = moment.utc(e.end).endOf(scale).add(1, 'milliseconds');
 
-    var segments = {};
-
-    // Clone our events so that we're not modifying the original
-    // objects. Loop through them, inserting the events into the
-    // corresponding segments
-    _.each(_.clone(events), e => {
-      
-      // Calculate the duration of the event; this determines
-      // how many segments it will be in
-      var startMoment = moment.utc(e[options.startAttribute]).startOf(scale);
-      var endMoment = moment.utc(e[options.endAttribute]).endOf(scale).add(1, 'milliseconds');
-
-      // For each duration, add the event to the corresponding segment
-      var segmentStart;
-      for(var i = 0, duration = endMoment.diff(startMoment, scale); i < duration; i++) {
-        segmentStart = moment.utc(startMoment).add(i, scale).unix();
-        if (!segments[segmentStart]) { segments[segmentStart] = []; }
-        segments[segmentStart].push(_.clone(e));
-      }
-    });
+        // For each duration, add the event to the corresponding segment
+        var segmentStart;
+        for(var i = 0, duration = endMoment.diff(startMoment, scale); i < duration; i++) {
+          segmentStart = moment.utc(startMoment).add(i, scale).unix();
+          if (!segments[segmentStart]) { segments[segmentStart] = []; }
+          segments[segmentStart].push(e.original);
+        }
+      });      
     return segments;
   }
 };
